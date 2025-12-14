4173c9e022400d1fa59aab4ae86e4e1d145fde41
dbgfreelanceprojects <dbgfreelanceprojects@gmail.com>
2025-12-14 13:11:30 +0100

    Estructura inicial del proyecto Home Sweet Home

---
+++ backend/hsh/src/main/java/es/dbg/hsh/HomeSweetHomeApplication.java
@@ -0,0 +1,13 @@
+package es.dbg.hsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSweetHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSweetHomeApplication.class, args);
+	}
+
+}
